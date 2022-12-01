8a4eab305426868e1f0b17e39000eb3be1f167a8
hamster197 <hamster197@mail.ru>
2022-12-01 16:03:30 +0300

    fix

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def v404_view(request, exception=None, template_name='404.html'):
+def v404_view(request, exception=None, ):
     return render(request, '404.html')
